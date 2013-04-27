486f515a6ac7b8d1bf2fb7842544305b95f4d128
Aidan Feldman <aidan.feldman@gmail.com>
2013-04-26 23:56:28 -0400

    vary color based on vote margin

--- Gemfile
+++ Gemfile
@@ -2,9 +2,12 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 
+gem 'color'
+
 group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails', '~> 2.0'
+  gem 'debugger'
 end
 
 group :production do
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,18 @@
 module QuestionsHelper
+  def question_color(question)
+    if question.positive
+      hue = 360 # red
+    else
+      hue = 240 # blue
+    end
+
+    num_yes_votes = question.yes_votes.size
+    num_no_votes = question.no_votes.size
+    total = num_yes_votes + num_no_votes
+
+    # grey if num_yes_votes == num_no_votes, brighter as margin increases in either direction
+    saturation = ((num_yes_votes - num_no_votes).abs.to_f / (total / 2.0)) * 100
+
+    Color::HSL.new(hue, saturation, 50)
+  end
 end
