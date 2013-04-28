35ceffd9fcdb7320bb4838b738c5d5b87787c63b
ashaegupta <ashaegupta@gmail.com>
2013-04-28 14:14:49 -0400

    Leaving color function unmodified

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -8,8 +8,7 @@ module QuestionsHelper
 
     num_yes_votes = question.yes_votes.size
     num_no_votes = question.no_votes.size
-    num_undecided_votes = question.undecided_votes.size
-    total = num_yes_votes + num_no_votes + num_undecided_votes
+    total = num_yes_votes + num_no_votes
 
     # grey if num_yes_votes == num_no_votes, brighter as margin increases in either direction
     saturation = ((num_yes_votes - num_no_votes).abs.to_f / (total / 2.0)) * 100
