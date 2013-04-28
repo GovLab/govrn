545b93027351f48310bc20b57c69508c63f5ae07
ashaegupta <ashaegupta@gmail.com>
2013-04-28 14:12:08 -0400

    Added undecided and non majority stats to list view

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,14 +1,15 @@
 module QuestionsHelper
   def question_color(question)
     if question.positive
-      hue = 360 # red
-    else
       hue = 240 # blue
+    else
+      hue = 360 # red
     end
 
     num_yes_votes = question.yes_votes.size
     num_no_votes = question.no_votes.size
-    total = num_yes_votes + num_no_votes
+    num_undecided_votes = question.undecided_votes.size
+    total = num_yes_votes + num_no_votes + num_undecided_votes
 
     # grey if num_yes_votes == num_no_votes, brighter as margin increases in either direction
     saturation = ((num_yes_votes - num_no_votes).abs.to_f / (total / 2.0)) * 100
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,12 +4,16 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :responses
 
+  def RESPONSE_OPTIONS
+    ['yes', 'no', 'undecided']
+  end
+
   def yes_votes
-    self.responses.where(vote: 'yes')
+    self.responses.where(vote: self.RESPONSE_OPTIONS[0])
   end
 
   def no_votes
-    self.responses.where(vote: 'no')
+    self.responses.where(vote: self.RESPONSE_OPTIONS[1])
   end
 
   def undecided_votes
@@ -24,7 +28,24 @@ class Question < ActiveRecord::Base
     self.no_votes.size.to_f / self.responses.size
   end
 
+  def undecided_fraction
+    self.undecided_votes.size.to_f / self.responses.size
+  end
+
   def positive
     self.yes_votes.size >= self.no_votes.size
+  end
+
+  def votes_by_type
+    [self.yes_votes.size, self.no_votes.size, self.undecided_votes.size]
+  end
+
+  def majority_response
+    self.votes_by_type.max
+  end
+
+  def majority_response_type
+    index = self.votes_by_type.index(self.majority_response)
+    self.RESPONSE_OPTIONS[index]  ## if more than one max, will return the first in keys array
   end
 end
